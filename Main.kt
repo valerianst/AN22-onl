a80544229fb8dd0c4499b4a71a0098f14faf3859
valerianst <105708916+valerianst@users.noreply.github.com>
2025-09-07 17:25:44 +0300

    дз 3 №1

--- Main.kt
+++ Main.kt
@@ -2,13 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     println ("Введите число")
-    val number: Int? = readln().toIntOrNull()
- if (number != null) {
-     if (number == 2) {
-         println("Число простое") }
-     else if   (number % 2 == 0 )  println ("Число не является простым")
-    else if (number <= 1) println ("Число не является простым")
-    else println ("Число простое")
-        }
+    val number = readln()
+    if (isPalindromeString(number)) {
+        println("$number - палиндром")
+    } else {
+        println("$number - не палиндром")
     }
 
+    }
+fun isPalindromeString(str: String): Boolean {
+    val cleanStr = str.replace("\\s".toRegex(), "")
+    return cleanStr == cleanStr.reversed()
+}
